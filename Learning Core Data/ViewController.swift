9fe096004005f9d2b5f3f980784c5c2fac0f0604
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-13 13:42:42 -0400

    Persisting Data using CoreData

--- Learning Core Data/ViewController.swift
+++ Learning Core Data/ViewController.swift
@@ -11,7 +11,8 @@ import CoreData
 
 class ViewController: UITableViewController {
     
-    var items:[String] = []
+    var items:[NSManagedObject] = []
+    
     var cellID = "CellID"
 
     override func viewDidLoad() {
@@ -27,6 +28,18 @@ class ViewController: UITableViewController {
         
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
+        do{
+            items = try managedContext.fetch(fetchRequest)
+        }catch{
+            print("Could not load data from database \(error.localizedDescription)")
+        }
+    }
     @objc
     func addItem(_sender: AnyObject){
         
@@ -34,7 +47,7 @@ class ViewController: UITableViewController {
         let saveAction = UIAlertAction(title: "Save", style: .default) {  [unowned self] action in
             guard let textField = alertController.textFields?.first,
             let itemToAdd = textField.text else{return}
-            self.items.append(itemToAdd)
+            self.save(itemToAdd)
             self.tableView.reloadData()
         }
         let cancelAction =  UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
@@ -45,13 +58,30 @@ class ViewController: UITableViewController {
        
     }
     
+    func save(_ itemName: String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Item", in: managedContext)!
+        let item = NSManagedObject(entity: entity, insertInto: managedContext)
+        item.setValue(itemName, forKey: "itemName")
+        
+        do{
+            try managedContext.save()
+            items.append(item)
+            tableView.reloadData()
+        }catch{
+            print("Could not save data ! \(error.localizedDescription)")
+            
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
         let item = items[indexPath.row]
-        cell.textLabel?.text = item
+        cell.textLabel?.text = item.value(forKeyPath: "itemName") as? String
         return cell
     }
 
